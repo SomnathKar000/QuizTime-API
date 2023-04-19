f343206b9c970b1d55bf32a238fbd2fe10609689
SomnathKar000 <somnathkar201901@gmail.com>
2023-04-19 22:44:28 +0530

    Basic structure is done

--- controllers/quizzesController.js
+++ controllers/quizzesController.js
@@ -1,8 +1,25 @@
 const CustomError = require("../error/error");
 
 const createQuiz = (req, res) => {
-  throw new CustomError("Custom error", 404);
-  //   res.status(200).json({ success: true, msg: "create quiz" });
+  res.status(200).json({ success: true, msg: "create quiz" });
 };
 
-module.exports = { createQuiz };
+const getAllActiveQuizzes = (req, res) => {
+  res.status(200).json({ status: true, msg: "Get all active quizes" });
+};
+
+const getAllquizzes = (req, res) => {
+  res.status(200).json({ status: true, msg: "Get all quizes" });
+};
+
+const getQuizResultById = (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({ status: true, msg: "Get quiz result by id", id });
+};
+
+module.exports = {
+  createQuiz,
+  getQuizResultById,
+  getAllquizzes,
+  getAllActiveQuizzes,
+};
--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,8 +1,19 @@
-const { createQuiz } = require("../controllers/quizzesController");
+const {
+  createQuiz,
+  getQuizResultById,
+  getAllquizzes,
+  getAllActiveQuizzes,
+} = require("../controllers/quizzesController");
 const express = require("express");
 
 const router = express.Router();
 
 router.route("/").post(createQuiz);
+
+router.route("/active").get(getAllActiveQuizzes);
+
+router.route("/:id/result").get(getQuizResultById);
+
+router.route("/all").get(getAllquizzes);
 
 module.exports = router;

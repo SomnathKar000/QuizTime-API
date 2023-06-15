52ceb17bd78c5841411778087209355287076632
SomnathKar000 <somnathkar201901@gmail.com>
2023-06-15 17:28:12 +0530

    new Changes

--- db/connect.js
+++ db/connect.js
@@ -4,7 +4,7 @@ const connection = mySql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER_NAME,
   password: process.env.DB_PASSWORD,
-  database: "quiz_data",
+  database: process.env.DB_NAME,
 });
 
 module.exports = connection;
